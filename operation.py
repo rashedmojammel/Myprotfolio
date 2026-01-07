bce3119cdb6c635a93c789598757f0a5ec69bb2b
rashedmojammel <rashedmojamme56@gmail.com>
2026-01-08 00:26:58 +0600

    Banner content header and nav

---
+++ operation.py
@@ -0,0 +1,21 @@
+
+
+score = int(input("Enter your score :"))
+if score >= 90 and score <= 100 :
+    print("Your Grade is A+")
+elif score >= 80 and score < 89 :
+    print("Your Grade is A")
+elif score >= 70 and score <= 79 :
+    print("Your Grade is B")
+elif score >= 60 and score <= 69 :
+    print("Your Grade is D")
+elif score >= 50 and score <= 59 :
+    print("Your Grade is E")
+elif score >= 40 and score <= 49 :
+    print("Your Grade is E-")
+else :
+ print ("Your Grade is F")
+    
+
+
+
